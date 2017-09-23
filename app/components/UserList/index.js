f1dce18f28f8fda7d24ba256e13a82e3512e779a
Tam Pham <tampham47@live.com>
2017-09-23 08:29:17 +0700

    d

--- app/components/UserList/index.js
+++ app/components/UserList/index.js
@@ -17,7 +17,6 @@ class UserList extends Component {
 
   componentDidMount() {
     window.addEventListener('goingsunny', function(e) {
-      console.log('goingsunny', e.detail);
       this.setState({
         clients: e.detail.message.clients
       });
