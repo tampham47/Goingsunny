e3570e077b3fcd8e09559f427641fcec01c18c17
Tan Nguyen <nguyentanuit@gmail.com>
2017-10-06 07:47:44 +0700

    add feature payment-via-momo

--- app/components/Payment/index.js
+++ app/components/Payment/index.js
@@ -4,15 +4,48 @@ import { Link } from 'react-router'
 import Helmet from 'react-helmet'
 
 import Header from 'core/Header';
+import iconQrCode from './ico-qrcode.svg';
 import styles from './styles.css';
 
 
+const PaymentInfo = () => (
+  <div className={styles.paymentInfo}>
+    <div className={styles.wrapQRCode}>
+      <div className={styles.qrCode}>
+        <img className={styles.icon} src={iconQrCode} />
+      </div>
+    </div>
+    <div className={styles.wrapContent}>
+      <div className={styles.phone}>
+        Phone: +841643-652-922
+      </div>
+      <div className={styles.note}>
+        Vui lòng nhắn vào khung bên dưới sau khi đã chuyển tiền thành công. Xin cảm ơn!
+      </div>
+    </div>
+  </div>
+);
 class Payment extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShowPaymentInfo: false,
+    };
+  }
+
   render() {
     return (
       <section className={styles.main}>
-        <Header title="Payment" />
-        <p>Consectetur consectetur elit ex magna. Aute cupidatat qui adipisicing nulla qui. Duis consectetur irure consectetur laborum consectetur ut ad nostrud eiusmod non.</p>
+        <h5>Payment</h5>
+        <div className={styles.payment}>
+          <button
+            className="button-primary"
+            onClick={() => this.setState({isShowPaymentInfo: !this.state.isShowPaymentInfo})}
+          >
+            Buy us a beer
+          </button>
+          {this.state.isShowPaymentInfo ? PaymentInfo() : null}
+        </div>
       </section>
     )
   }
