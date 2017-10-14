850b6da6a7caf4fdb1b26d7d6ca88960faee15b8
Tan Nguyen <TanMimosaTek@Tans-MacBook-Pro.local>
2017-10-14 15:04:57 +0700

    move modal from RelatedPost to Youtube

--- app/components/RelatedPost/Youtube/index.js
+++ app/components/RelatedPost/Youtube/index.js
@@ -1,25 +1,53 @@
-import React, { Component }  from 'react';
+import React, { Component } from 'react';
+
+import Modal from 'core/Modal';
+import Detail from './Detail';
 
 import iconYoutube from './ico-youtube.svg';
 import styles from './styles.css';
+class YoutubeDocument extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isShowDetail: false
+    };
+    this.handleOnClick = this.handleOnClick.bind(this);
+    this.handleDissmiss = this.handleDissmiss.bind(this);
+  }
 
-const YoutubeDocument = ({ item, handleOnClick }) => (
-  <div className={styles.main} key={item._id} onClick={handleOnClick}>
-    <div className={styles.wrapIcon}>
-      <img className={styles.icon} src={iconYoutube} />
-    </div>
-    <div className={styles.wrapContent}>
-      <div className={styles.title}>
-        {item.title}
+  handleOnClick() {
+    this.setState({ isShowDetail: !this.state.isShowDetail });
+  }
+
+  handleDissmiss() {
+    this.setState({ isShowDetail: false });
+  }
+
+  render() {
+    const { item } = this.props;
+    return (
+      <div className={styles.main} onClick={this.handleOnClick}>
+        <div className={styles.wrapIcon}>
+          <img className={styles.icon} src={iconYoutube} />
+        </div>
+          <div className={styles.wrapContent}>
+            <div className={styles.title}>
+              {item.title}
+            </div>
+            <div className={styles.description}>
+              {item.description}
+            </div>
+            <div className={styles.link}>
+              {item.url}
+            </div>
+          </div>
+        <Modal isShow={this.state.isShowDetail} dismiss={this.handleDissmiss}>
+          <Detail isShow={this.state.isShowDetail} item={item}/>
+        </Modal>
       </div>
-      <div className={styles.description}>
-        {item.description}
-      </div>
-      <div className={styles.link}>
-        {item.url}
-      </div>
-    </div>
-  </div>
-);
+  );
+  }
+}
 
 export default YoutubeDocument;
+
--- app/components/RelatedPost/index.js
+++ app/components/RelatedPost/index.js
@@ -2,34 +2,13 @@ import React, {Component} from 'react';
 import styles from './styles.css';
 
 import Header from 'core/Header';
-import Modal from 'core/Modal';
 
 import Pdf from './Pdf';
 import Link from './Link';
 import Youtube from './Youtube';
-import Detail from './Youtube/Detail';
-
 
 class RelatedPost extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isShowDetail: false,
-      currentItem: '',
-    };
-    this.handleDissmiss = this.handleDissmiss.bind(this);
-  }
-
-  handleOnClick(item) {
-    this.setState({ isShowDetail: !this.state.isShowDetail });
-    this.setState({ currentItem: item });
-  }
-
-  handleDissmiss() {
-    this.setState({ isShowDetail: false });
-  }
-
-  render(){
+  render() {
     const { model } = this.props;
     return (
       <section className={styles.main}>
@@ -39,14 +18,11 @@ class RelatedPost extends Component {
             const Elm = item.type === 'pdf' ? Pdf : item.type === 'youtube' ? Youtube : Link;
             return (
               <li key={item._id} className={styles.item}>
-                <Elm item={item} handleOnClick={this.handleOnClick.bind(this, item)}/>
+                <Elm item={item}/>
               </li>
             );
           })}
         </ul>
-        <Modal isShow={this.state.isShowDetail} dismiss={this.handleDissmiss}>
-          <Detail isShow={this.state.isShowDetail} item={this.state.currentItem}/>
-        </Modal>
       </section>
     );
   }
