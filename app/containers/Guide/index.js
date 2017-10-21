be6679ecca36fbf20e8619a20e7bb4e64cc1b62b
Tam Pham <tampham47@live.com>
2017-10-21 15:37:09 +0700

    d

--- app/containers/Guide/index.js
+++ app/containers/Guide/index.js
@@ -4,6 +4,11 @@ import { Link } from 'react-router';
 import Helmet from 'react-helmet';
 
 import styles from './styles.css';
+import facebook from './imgs/Facebook.png';
+import appearin from './imgs/Appearin.png';
+import friends from './imgs/Friend.png';
+import join from './imgs/Join.png';
+import match from './imgs/Match.png';
 
 
 class Main extends Component {
@@ -16,7 +21,54 @@ class Main extends Component {
     return (
       <section className={styles.main}>
         <div className="container">
-          <p>Lorem incididunt velit nisi incididunt voluptate irure dolor. Laborum velit eu excepteur velit laborum excepteur pariatur sunt. Excepteur deserunt ad dolore sunt officia. Magna do aute adipisicing consectetur occaecat ipsum magna ea consequat voluptate minim aliqua. Adipisicing fugiat non consequat cillum Lorem voluptate esse in nisi id.</p>
+          <h3 className={styles.title}>Hướng dẫn sử dụng Goingsunny</h3>
+
+          <div className={styles.section}>
+            <h6 className={styles.step}>1. Đăng nhập thông qua facebook</h6>
+            <p className={styles.content}>
+              Goingsunny sẽ không đăng bất kỳ thông tin gì lên Facebook của bạn.
+            </p>
+            <img src={facebook} alt="facebook"/>
+          </div>
+
+          <div className={styles.section}>
+            <h6 className={styles.step}>2. Tham gia nói chuyện</h6>
+            <p className={styles.content}>
+              Going sunny sẽ bắt đầu kết nối 2 người vào 1 nhóm từ 8PM - 10PM mỗi ngày.
+            </p>
+            <p className={styles.content}>
+              Để tham gia nói chuyện trực tuyến hãy nhấn vào nút <strong>Tham Gia Nói Chuyện</strong> bên dưới.
+            </p>
+            <p className={styles.content}>
+              Hãy thân thiện, bằng cách duy trì cuộc nói chuyện ít nhất 15 phút, sau đó bạn có thể trở lại going sunny và nhấp vào nút `Tham gia nói chuyện` một lần nữa.
+              Sau mỗi 5 phút, hệ thống sẽ lại kết nối bạn với 1 người khác, và cứ như vậy…
+            </p>
+            <img src={join} alt="join"/>
+          </div>
+
+          <div className={styles.section}>
+            <h6 className={styles.step}>3. Xuất hiện trên danh sách chờ</h6>
+            <p className={styles.content}>
+              Sau khi bạn nhấp vào nút <strong>Tham gia nói chuyện</strong> thông tin của bạn sẽ được xuất hiện trên danh sách chờ, cùng danh sách những người sẽ cùng bạn tham gia nói chyện.
+            </p>
+            <img src={friends} alt="friends"/>
+          </div>
+
+          <div className={styles.section}>
+            <h6 className={styles.step}>4. Show time</h6>
+            <p className={styles.content}>
+              Từ 8PM - 10PM mỗi ngày, mỗi 5 phút hệ thống sẽ kết nối những người tham gia lại với nhau, 2 người một nhóm. Hãy nhất vào nút <strong>GO</strong> để bắt đầu cuộc gọi video nhé.
+            </p>
+            <img src={match} alt="match"/>
+          </div>
+
+          <div className={styles.section}>
+            <h6 className={styles.step}>5. Appear.in</h6>
+            <p className={styles.content}>
+              Sau khi nhấn vào nút <strong>GO</strong> bạn sẽ được dẫn tới trang appearin. Hãy chuẩn bị mic và webcam để có một cuộc nói chuyện vui vẻ nhé. Hãy tôn trọng bạn bè của mình, bằng cách ở trong cuộc hội thoại <strong>ít nhất 15 phút</strong>, và đừng quên nói <strong>lời tạm biệt</strong> khi rời phòng nhé.
+            </p>
+            <img src={appearin} alt="appearin"/>
+          </div>
         </div>
       </section>
     )
