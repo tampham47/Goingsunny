60c8fbf3b47a5dd484d7e86e44e7a1adbf1e66df
Tam Pham <tampham47@live.com>
2017-10-21 16:58:07 +0700

    using https instead of http

--- app/config/index.js
+++ app/config/index.js
@@ -1,6 +1,6 @@
 let config = {
   API_BASE_URL: process.env.API_BASE_URL || 'https://api.goingsunny.com/api/v1',
-  API_IMAGE_URL: 'http://api.goingsunny.com/uploads',
+  API_IMAGE_URL: 'https://api.goingsunny.com/uploads',
   MQTT_URL: process.env.MQTT_URL || 'wss://broker.goingsunny.com',
   // MQTT_URL: process.env.MQTT_URL || 'ws://localhost:5551',
 }
