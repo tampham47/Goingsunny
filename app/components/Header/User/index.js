c53196a745a6905c6e3f4ebcc91ca2fcd5113b3c
Tan Nguyen <TanMimosaTek@Tans-MacBook-Pro.local>
2017-10-28 08:07:07 +0700

    add mark mentor

--- app/components/Header/User/index.js
+++ app/components/Header/User/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import Helmet from 'react-helmet';
 
+import iconCrown from './ico-crown.svg';
+
 import styles from './styles.css';
 
 
@@ -9,10 +11,13 @@ class User extends Component {
   render() {
     const user = this.props.model;
 
+    console.log('isMentor', user.isMentor);
+
     return (
       <span className={styles.main}>
         <span className={styles.avatar}>
-          <img src={`https://avatars.io/facebook/${user.providerId}`} alt=''/>
+          <img className={styles.avatarImage} src={`https://avatars.io/facebook/${user.providerId}`} alt=''/>
+          {user.isMentor ? null: <img className={styles.avatarIcon} src={iconCrown} alt=''/> }
         </span>
         <span className={styles.name}>{user.displayName}</span>
       </span>
--- app/components/UserList/UserInfo/index.js
+++ app/components/UserList/UserInfo/index.js
@@ -2,16 +2,21 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import Helmet from 'react-helmet';
 
+import iconCrown from './ico-crown.svg';
+
 import styles from './styles.css';
 
 
 const UserInfo = (props) => {
   const user = props.model || {};
-
   return (
     <span className={styles.main}>
       <span className={styles.avatar}>
-        <img src={`https://avatars.io/facebook/${user.providerId}`} alt=''/>
+        <img
+          className={styles.avatarImage}
+          src={`https://avatars.io/facebook/${user.providerId}`} alt=''
+        />
+        {user.isMentor ? <img className={styles.avatarIcon} src={iconCrown} alt=''/> : null }
       </span>
       <span className={styles.content}>
         <span>{user.displayName || 'User Name'}</span>
